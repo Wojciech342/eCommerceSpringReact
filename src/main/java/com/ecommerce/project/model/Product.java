7a0e9fbaf7039e10d89b6d6a9986e74e65e411c7
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-03-01 15:45:27 +0100

    added product module with controller, DTO, response, repository and service

---
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,29 @@
+package com.ecommerce.project.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double specialPrice;
+
+}
---
+++ src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -0,0 +1,16 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private List<ProductDTO> content;
+}
