3f01d8ce953014d0642027ddfae877f93e621a7e
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-03-15 20:28:29 +0100

    formatting changes added

--- src/main/java/com/ecommerce/project/repository/CartRepository.java
+++ src/main/java/com/ecommerce/project/repository/CartRepository.java
@@ -3,9 +3,8 @@ package com.ecommerce.project.repository;
 import com.ecommerce.project.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
+
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("SELECT c FROM Cart c where c.user.email = :email")
