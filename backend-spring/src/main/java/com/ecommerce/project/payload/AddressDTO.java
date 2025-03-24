9d54dcd0eee613230b15073f4ed50b1d1cb2ae99
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-03-24 18:13:32 +0100

    changed folder structure to backend-spring

---
+++ backend-spring/src/main/java/com/ecommerce/project/payload/AddressDTO.java
@@ -0,0 +1,17 @@
+package com.ecommerce.project.payload;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
+    private Long addressId;
+    private String street;
+    private String buildingName;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
+}
