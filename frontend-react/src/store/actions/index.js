fbc32cf9bdf4ca03271c422e7e0f5c60d5ad554e
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-02 23:44:56 +0200

    add increase and decrease cart quantity methods

--- frontend-react/src/store/actions/index.js
+++ frontend-react/src/store/actions/index.js
@@ -73,3 +73,43 @@ export const addToCart =
             toast.error('Out of stock');
         }
     };
+export const increaseCartQuantity =
+    (data, toast, currentQuantity, setCurrentQuantity) =>
+    (dispatch, getState) => {
+        // Find the product
+        const { products } = getState().products;
+
+        const getProduct = products.find(
+            (item) => item.productId === data.productId
+        );
+
+        const isQuantityExist = getProduct.stock >= currentQuantity + 1;
+
+        if (isQuantityExist) {
+            const newQuantity = currentQuantity + 1;
+            setCurrentQuantity(newQuantity);
+
+            dispatch({
+                type: 'ADD_CART',
+                payload: { ...data, quantity: newQuantity + 1 },
+            });
+            localStorage.setItem(
+                'cartItems',
+                JSON.stringify(getState().carts.cart)
+            );
+        } else {
+            toast.error('Quantity Reached to Limit');
+        }
+    };
+
+export const decreaseCartQuantity =
+    (data, newQuantity) => (dispatch, getState) => {
+        dispatch({
+            type: 'ADD_CART',
+            payload: { ...data, quantity: newQuantity },
+        });
+        localStorage.setItem(
+            'cartItems',
+            JSON.stringify(getState().carts.cart)
+        );
+    };
