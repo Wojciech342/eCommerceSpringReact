4fccb9329a038855260324bdddac16baa3413f81
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-04 11:01:58 +0200

    add log out reducer

--- frontend-react/src/store/reducers/authReducer.js
+++ frontend-react/src/store/reducers/authReducer.js
@@ -5,8 +5,11 @@ const initialState = {
 
 export const authReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'LOGIN_USER':
+        case 'LOG_IN_USER':
             return { ...state, user: action.payload };
+
+        case 'LOG_OUT_USER':
+            return { user: null, address: null };
 
         default:
             return state;
