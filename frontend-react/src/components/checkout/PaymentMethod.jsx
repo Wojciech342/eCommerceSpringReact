df3a73aad17d8476ff1b6b7df9708eb94b7c57c0
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-16 06:00:15 +0200

    fix formatting

--- frontend-react/src/components/checkout/PaymentMethod.jsx
+++ frontend-react/src/components/checkout/PaymentMethod.jsx
@@ -1,6 +1,11 @@
-import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import {
+    FormControl,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+} from '@mui/material';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { addPaymentMethod, createUserCart } from '../../store/actions';
 
 const PaymentMethod = () => {
@@ -17,39 +22,43 @@ const PaymentMethod = () => {
                     quantity: item.quantity,
                 };
             });
-            
+
             dispatch(createUserCart(sendCartItems));
         }
     }, [dispatch, cartId]);
 
     const paymentMethodHandler = (method) => {
         dispatch(addPaymentMethod(method));
-    }
-  return (
-    <div className='max-w-md mx-auto p-5 bg-white shadow-md rounded-lg mt-16 border'>
-        <h1 className='text-2xl font-semibold mb-4'>Select Payment Method</h1>
-        <FormControl>
-            <RadioGroup
-                aria-label="payment method"
-                name="paymentMethod"
-                value={paymentMethod}
-                onChange={(e) => paymentMethodHandler(e.target.value)}
-            >
-                <FormControlLabel 
-                    value="Stripe" 
-                    control={<Radio color='primary' />} 
-                    label="Stripe" 
-                    className='text-gray-700'/>
+    };
+    return (
+        <div className='max-w-md mx-auto p-5 bg-white shadow-md rounded-lg mt-16 border'>
+            <h1 className='text-2xl font-semibold mb-4'>
+                Select Payment Method
+            </h1>
+            <FormControl>
+                <RadioGroup
+                    aria-label='payment method'
+                    name='paymentMethod'
+                    value={paymentMethod}
+                    onChange={(e) => paymentMethodHandler(e.target.value)}
+                >
+                    <FormControlLabel
+                        value='Stripe'
+                        control={<Radio color='primary' />}
+                        label='Stripe'
+                        className='text-gray-700'
+                    />
 
-                <FormControlLabel 
-                    value="Paypal" 
-                    control={<Radio color='primary' />} 
-                    label="Paypal" 
-                    className='text-gray-700'/>
-            </RadioGroup>
-        </FormControl>
-    </div>
-  )
-}
+                    <FormControlLabel
+                        value='Paypal'
+                        control={<Radio color='primary' />}
+                        label='Paypal'
+                        className='text-gray-700'
+                    />
+                </RadioGroup>
+            </FormControl>
+        </div>
+    );
+};
 
-export default PaymentMethod
+export default PaymentMethod;
