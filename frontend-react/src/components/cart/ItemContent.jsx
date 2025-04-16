8c74448bf43573bf18a3f56099c7128a463354d1
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-16 06:26:23 +0200

    fix bug with wrong image src

--- frontend-react/src/components/cart/ItemContent.jsx
+++ frontend-react/src/components/cart/ItemContent.jsx
@@ -59,13 +59,10 @@ const ItemContent = ({
 
                 <div className='md:w-36 sm:w-24 w-12'>
                     <img
-                        src={`${
-                            import.meta.env.VITE_BACK_END_URL
-                        }/images/${image}`}
+                        src={image}
                         alt={productName}
                         className='md:h-36 sm:h-24 h-12 w-full object-cover rounded-md'
                     />
-
                     <div className='flex items-start gap-5 mt-3'>
                         <button
                             onClick={() =>
