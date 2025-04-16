22a5923acf4c210408d3bfec654ddb87a9eb327c
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-16 06:04:08 +0200

    add reducer cases for button and category loading

--- frontend-react/src/store/reducers/errorReducer.js
+++ frontend-react/src/store/reducers/errorReducer.js
@@ -3,6 +3,7 @@ const initialState = {
     errorMessage: null,
     categoryLoader: false,
     categoryError: null,
+    btnLoader: false,
 };
 
 export const errorReducer = (state = initialState, action) => {
@@ -13,12 +14,22 @@ export const errorReducer = (state = initialState, action) => {
                 isLoading: true,
                 errorMessage: null,
             };
+        case 'BUTTON_LOADER':
+            return {
+                ...state,
+                btnLoader: true,
+                errorMessage: null,
+                categoryError: null,
+            };
 
         case 'IS_SUCCESS':
             return {
                 ...state,
                 isLoading: false,
                 errorMessage: null,
+                btnLoader: false,
+                categoryError: null,
+                categoryLoader: false,
             };
 
         case 'IS_ERROR':
@@ -26,6 +37,8 @@ export const errorReducer = (state = initialState, action) => {
                 ...state,
                 isLoading: false,
                 errorMessage: action.payload,
+                btnLoader: false,
+                categoryLoader: false,
             };
 
         case 'CATEGORY_SUCCESS':
